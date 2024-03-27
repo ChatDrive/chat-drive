7467082552a1afce6d88982097aaf081e9358089
Jaqea <1601676692@qq.com>
2024-03-27 19:42:13 +0800

    🌈 other(custom): add app fullscreen config

---
+++ src/layouts/index.ts
@@ -0,0 +1,6 @@
+// import { computed, watch } from 'vue'
+// import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+// import { useAppStore, useThemeStore } from '@/store'
+
+// type LayoutMode = 'vertical' | 'horizontal'
+// type LayoutHeaderProps
